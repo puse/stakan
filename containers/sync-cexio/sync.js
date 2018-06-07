396565f2267cd91c09dc1046edfbad4568ec13d1
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-06-07 15:04:39 +0400

    Setup pipeline with observables

---
+++ containers/sync-cexio/sync.js
@@ -0,0 +1,65 @@
+const {
+  Observable,
+  Subject
+} = require('rxjs/Rx')
+
+const {
+  assoc,
+  compose,
+  reduce,
+  pluck,
+  applySpec,
+  prop,
+  last
+} = require('ramda')
+
+const {
+  isNotEmpty
+} = require('ramda-adjunct')
+
+const Client = require('./lib/client')
+const Store = require('./lib/store')
+
+const OBH = require('./lib/helpers')
+
+const client = new Client('btc-usd')
+const store = new Store()
+
+const input$ = new Subject()
+
+const reset$ = Observable
+  .fromEvent(client, 'reset')
+
+const update$ = Observable
+  .fromEvent(client, 'update')
+  .bufferTime(25)
+  .filter(isNotEmpty)
+  .map(arr => {
+    const concatAll = reduce(OBH.concat, [])
+
+    const op = applySpec({
+      ev     : compose(prop('ev'), last),
+      broker : compose(prop('broker'), last),
+      symbol : compose(prop('symbol'), last),
+      bids   : compose(concatAll, pluck('bids')),
+      asks   : compose(concatAll, pluck('asks'))
+    })
+
+    return op(arr)
+  })
+
+reset$
+  .merge(update$)
+  .flatMap(x => {
+    return x.ev === 'reset'
+      ? store.resetOrderBook(x)
+      : store.updateOrderBook(x)
+  })
+  .subscribe(ingest)
+
+
+function ingest (x) {
+  console.log(x)
+}
+
+client.sync()
