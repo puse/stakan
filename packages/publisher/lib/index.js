2e637b01a975214f61857f4a10487faa12fca591
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-06-08 20:36:24 +0400

    Setup publisher

--- packages/publisher/lib/index.js
+++ packages/publisher/lib/index.js
@@ -1,8 +1,8 @@
-const debug = require('debug')('stakan:publisher')
+const debug = require('debug')('stakan:mqtt:publisher')
 
 const mqtt = require('mqtt')
 
-const { ReplaySubject } = require('rxjs')
+const { ReplaySubject } = require('rxjs/Rx')
 
 const getenv = require('getenv')
 
@@ -12,6 +12,9 @@ const { publish } = require('./actions')
  * Config
  */
 
+const REPLAY_BUFFER_SIZE = 1000
+const REPLAY_WINDOW_TIME = 1000
+
 /**
  * MQTT server url
  */
@@ -19,22 +22,55 @@ const { publish } = require('./actions')
 const MQTT_URL = getenv('MQTT_URL', 'mqtt://localhost:1883')
 
 /**
+ * Helpers
+ */
+
+function createReplay () {
+  const bufferSize = REPLAY_BUFFER_SIZE
+  const windowTime = REPLAY_WINDOW_TIME
+
+  return new ReplaySubject(bufferSize, windowTime)
+}
+
+/**
  * Publisher
  */
 
 class Publisher {
   constructor (url = MQTT_URL, opts = {}) {
-    const client = mqtt.connect(url)
+    debug('Connecting to %s', url)
 
-    const replay = new ReplaySubject()
+    this.client = mqtt.connect(url)
+    this.replay = createReplay()
 
-    client.on('connect', _ => {
-      debug('Publisher connected')
-      replay.subscribe(payload => publish(client, payload))
+    this._setup()
+  }
+
+  _setup () {
+    const { client, replay } = this
+
+    client.on('error', err => {
+      debug('Error: %s', err.message)
     })
 
-    this.client = client
-    this.replay = replay
+    client.on('connect', _ => {
+      debug('Connected')
+
+      //
+      replay.subscribe(publish(client))
+    })
+
+    client.on('close', _ => {
+      debug('Connection closed')
+    })
+
+    replay
+      .bufferTime(1000)
+      .subscribe(
+        arr => debug('Published %d messages', arr.length)
+      )
+
+    return this
   }
 
   publish (payload) {
