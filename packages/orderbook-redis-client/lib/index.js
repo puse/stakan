b673eb6be2464b44ad295f8c8dc4fada63357411
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-07-01 09:12:32 +0400

    Implement OBWATCH (tuned XREAD)

--- packages/orderbook-redis-client/lib/index.js
+++ packages/orderbook-redis-client/lib/index.js
@@ -3,6 +3,8 @@ const { readFileSync } = require('fs')
 const Redis = require('ioredis')
 
 const {
+  assoc,
+  fromPairs,
   compose,
   map,
   zipObj,
@@ -17,6 +19,7 @@ const setupScripts = require('./scripts')
  *
  */
 
+const { Command } = Redis
 
 /**
  * Utils
@@ -57,26 +60,62 @@ class Client extends Redis {
   }
 
   obdepth (key) {
-    const recover = zipObj(['price', 'amount'])
+    const toEntry = zipObj(['price', 'amount'])
 
     const parse = compose(
-      map(recover),
+      map(toEntry),
       splitEvery(2),
       map(Number)
     )
 
+    const recover = res => {
+      const [ asks, bids, rev ] = res
+
+      return {
+        rev,
+        asks: parse(asks),
+        bids: parse(bids)
+      }
+    }
+
     return this
       .OBDEPTH(key)
-      .then(res => {
-        const [ asks, bids, rev ] = res
+      .then(recover)
+  }
 
-        return {
-          rev,
-          asks: parse(asks),
-          bids: parse(bids)
-        }
-      })
+  obwatch (topic, id = '$', t = 1e3) {
+    const key = `${topic}:ob:log`
+    const args = ['BLOCK', t, 'streams', key, id]
+    const options = {
+      replyEncoding: 'utf8'
+    }
 
+    const cmd = new Command('xread', args, options)
+
+    const parseArr = compose(
+      fromPairs,
+      splitEvery(2)
+    )
+
+    const parseRow = pair => {
+      const [ id, arr ] = pair
+
+      const entry =  parseArr(arr)
+
+      return assoc('id', id, entry)
+    }
+
+    const recover = res => {
+      if (!res) return res
+
+      const arr = res[0][1]
+
+      return map(parseRow, res[0][1])
+    }
+
+    return this
+      .sendCommand(cmd)
+      .then(recover)
   }
 }
 
