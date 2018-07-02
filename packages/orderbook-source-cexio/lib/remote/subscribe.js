75e381df1f08b96f198b2547c523b20381bc52dd
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-07-02 14:57:24 +0400

    Separate remote functionality

---
+++ packages/orderbook-source-cexio/lib/remote/subscribe.js
@@ -0,0 +1,26 @@
+const debug = require('debug')('stakan:source:cexio')
+
+const send = require('./send')
+
+const { symbolToPair } = require('./helpers')
+
+const CHANNEL = 'order-book-subscribe'
+
+function subscribe (ws, symbol, depth = 15) {
+  debug('Subscribing to %s', symbol)
+
+  const pair = symbolToPair(symbol)
+
+  const payload = {
+    e: CHANNEL,
+    data: {
+      pair,
+      depth,
+      subscribe: true
+    }
+  }
+
+  return send(ws, payload, true)
+}
+
+module.exports = subscribe
---
+++ packages/orderbook-source-cexio/test/remote.spec.js
@@ -0,0 +1,17 @@
+import test from 'ava'
+
+import Remote from '../lib/remote'
+
+import createPool from '../lib/pool'
+
+const pool = createPool()
+
+test('send', async t => {
+  const ws = await pool.acquire()
+
+  await Remote
+    .subscribe(ws, 'btc-usd')
+    .then(console.log)
+})
+
+
