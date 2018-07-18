9459e6d77d15f38e1f12c037a3afa236b9a33ffd
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-07-18 21:41:38 +0400

    Add local router w/ l2*

--- containers/orderbook/lib/router.js
+++ containers/orderbook/lib/router.js
@@ -1,12 +1,12 @@
 const Router = require('koa-router')
 
-const { obdepth } = require('@stakan/orderbook-db-methods')
+const { l2depth } = require('@stakan/orderbook-db-methods')
 
 function read ({ redis } = {}) {
   return async ctx => {
     const db = redis || ctx.redis
 
-    ctx.body = await obdepth(db, ctx.params)
+    ctx.body = await l2depth(db, ctx.params)
   }
 }
 
