53a05abe42312e169f3c56b5271acaf037c4b14b
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-12-25 02:52:39 +0400

    Separate remote-connector-bitfinex

---
+++ containers/remote-connector-bitfinex/lib/subscriber-db.js
@@ -0,0 +1,47 @@
+const debug = require('debug')('stakan:l2:db')
+
+const { Subscriber } = require('rxjs/Rx')
+
+const {
+  l2add,
+  l2commit
+} = require('@stakan/db-methods')
+
+/**
+ * Ingest patches
+ *
+ * @param {Redis} db
+ *
+ * @return {Rx.Subscriber}
+ */
+
+/**
+ * TODO: handle db connection
+ */
+
+function Sink (db) {
+  debug('Initialize DB subscriber')
+
+  const next = patch => {
+    const commit = _ => {
+      return l2commit(db, patch)
+    }
+
+    const { session, rows } = patch
+
+    return l2add(db, patch, session, rows)
+      .then(commit)
+  }
+
+  const error = err => debug('Error: ', err.message)
+
+  const complete = _ => debug('Complete')
+
+  return Subscriber.create(next, error, complete)
+}
+
+/**
+ * Expose
+ */
+
+module.exports = Sink
